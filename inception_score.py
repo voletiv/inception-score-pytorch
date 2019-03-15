716473dd9cd7b253dba4305cbe45173bda1381b7
Vikram Voleti <vikky2904@gmail.com>
2019-03-15 00:23:34 -0400

    My edits

--- inception_score.py
+++ inception_score.py
@@ -3,6 +3,7 @@ from torch import nn
 from torch.autograd import Variable
 from torch.nn import functional as F
 import torch.utils.data
+import tqdm
 
 from torchvision.models.inception import inception_v3
 
@@ -41,13 +42,14 @@ def inception_score(imgs, cuda=True, batch_size=32, resize=False, splits=1):
         if resize:
             x = up(x)
         x = inception_model(x)
-        return F.softmax(x).data.cpu().numpy()
+        return F.softmax(x, dim=1).data.cpu().numpy()
 
     # Get predictions
     preds = np.zeros((N, 1000))
 
-    for i, batch in enumerate(dataloader, 0):
-        batch = batch.type(dtype)
+    for i, batch in tqdm.tqdm(enumerate(dataloader), total=len(dataloader)):
+        # import pdb; pdb.set_trace()
+        batch = batch[0].type(dtype)
         batchv = Variable(batch)
         batch_size_i = batch.size()[0]
 
@@ -56,7 +58,7 @@ def inception_score(imgs, cuda=True, batch_size=32, resize=False, splits=1):
     # Now compute the mean kl-div
     split_scores = []
 
-    for k in range(splits):
+    for k in tqdm.tqdm(range(splits)):
         part = preds[k * (N // splits): (k+1) * (N // splits), :]
         py = np.mean(part, axis=0)
         scores = []
@@ -67,29 +69,38 @@ def inception_score(imgs, cuda=True, batch_size=32, resize=False, splits=1):
 
     return np.mean(split_scores), np.std(split_scores)
 
+
 if __name__ == '__main__':
-    class IgnoreLabelDataset(torch.utils.data.Dataset):
-        def __init__(self, orig):
-            self.orig = orig
-
-        def __getitem__(self, index):
-            return self.orig[index][0]
-
-        def __len__(self):
-            return len(self.orig)
 
     import torchvision.datasets as dset
     import torchvision.transforms as transforms
 
-    cifar = dset.CIFAR10(root='data/', download=True,
-                             transform=transforms.Compose([
-                                 transforms.Scale(32),
+    im_dset = dset.ImageFolder(root='/home/voletiv/Downloads/ICASSP/faces', transform=transforms.Compose([
                                  transforms.ToTensor(),
-                                 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-                             ])
-    )
+                                 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+                    )
 
-    IgnoreLabelDataset(cifar)
+    print (inception_score(im_dset, cuda=False, batch_size=32, resize=True, splits=10))
 
-    print ("Calculating Inception Score...")
-    print (inception_score(IgnoreLabelDataset(cifar), cuda=True, batch_size=32, resize=True, splits=10))
+    # class IgnoreLabelDataset(torch.utils.data.Dataset):
+    #     def __init__(self, orig):
+    #         self.orig = orig
+
+    #     def __getitem__(self, index):
+    #         return self.orig[index][0]
+
+    #     def __len__(self):
+    #         return len(self.orig)
+
+    # cifar = dset.CIFAR10(root='data/', download=True,
+    #                          transform=transforms.Compose([
+    #                              transforms.Scale(32),
+    #                              transforms.ToTensor(),
+    #                              transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    #                          ])
+    # )
+
+    # IgnoreLabelDataset(cifar)
+
+    # print ("Calculating Inception Score...")
+    # print (inception_score(IgnoreLabelDataset(cifar), cuda=True, batch_size=32, resize=True, splits=10))
